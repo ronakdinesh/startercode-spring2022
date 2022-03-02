f9488bf03e2b703c92e23f5b21b49cdfca50c77d
ronakdinesh <ronakhq@gmail.com>
2022-03-02 16:20:21 -0500

    draft q1

---
+++ midterm/q1a.py
@@ -0,0 +1,31 @@
+from mockmr import MockMR
+import random
+
+class q1(MockMR):
+def mapper_init(self): 
+    self.cache ={}
+def mapper(self, key, value):
+    LIMIT = 20
+    parts = list(([line.strip()]))[0]   
+    if parts != [Airline,Source,Destination,Stops]:
+        yield parts[0],parts[2]
+
+    if len(self.cache) > limit:
+            for i in self.cache:
+                yield (i, self.cache[i])
+            self.cache.clear()
+
+def mapper_final(self):
+    if len(self.cache) != 0:
+            for i in self.cache:
+                yield (i, self.cache[i])
+
+
+def reducer(self, key, values):
+    #keys: source airport, value: destination airport
+    yield key, sum(values)
+
+
+if __name__ == "__main__":
+
+    q1.run(trace=True)
