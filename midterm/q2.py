f24c60e30ac5af03e88c4786c54656cd75e504bf
ronakdinesh <ronakhq@gmail.com>
2022-03-02 17:59:29 -0500

    add

--- midterm/q2.py
+++ midterm/q2.py
@@ -1,16 +1,3 @@
-'''Question 3. The previous question asked you two write two separate programs, but why write two when you
-can write only one? Write one mapreduce job that, for each airport, counts the number of incoming and
-outgoing 
-ights. So, for example, if LAX has 3 outgoing 
-ights and 4 incoming 
-ights, one of the output
-lines should look like:
-LAX (3, 4)
-(don't worry so much about punctuation in the output le, the content is more important). Make sure your
-code is ecient (pretend the list of all airports is too large to t in memory). Think carefully about what
-messages the mapper needs to send so that the reducer can compute the right answer.
- Put your code in q2.py
- Make sure to add/commit/push. Submit to gradescope.'''
 
 from mockmr import MockMR
 import random
