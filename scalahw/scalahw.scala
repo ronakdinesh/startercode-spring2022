f5c0a23eaf6cb4a8cf5a47d5b50a5a0a083c0bac
Daniel Kifer <dkifer@cse.psu.edu>
2022-03-12 16:22:49 -0500

    hdfs and scala homeworks

---
+++ scalahw/scalahw.scala
@@ -0,0 +1,35 @@
+case class Neumaier(sum: Double, c: Double)
+
+object HW {
+
+   def q1_middle(x: Int, y: Int, z:Int) = {
+      //the types of the input parameters have been declared.
+      //you must do the same for the output type (see scala slides)
+      //do not use return statements.
+      (1,1)
+   }
+
+   def q2_interpolation(name: String, age: Int) = {
+      //the types of the input parameters have been declared.
+      //you must do the same for the output type (see scala slides)
+      //do not use return statements.
+      ""
+   }
+
+   def q3_polynomial(arr: Seq[Double]) = {
+      //the types of the input parameters have been declared.
+      //you must do the same for the output type (see scala slides)
+      //do not use return statements.
+      0.0
+   }
+
+   def q4_application(x: Int, y: Int, z: Int)(f: (Int, Int) => Int) = {
+      //the types of the input parameters have been declared.
+      //you must do the same for the output type (see scala slides)
+      //do not use return statements.
+      1
+   }
+
+   // create the rest of the functions yourself
+
+}
